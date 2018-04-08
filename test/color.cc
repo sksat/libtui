d039d00876b59c634a9f3392b36cd2c6422f1d3b
sksat <sksat.tec@gmail.com>
2018-04-08 13:24:27 +0900

    [ADD] color test

---
+++ test/color.cc
@@ -0,0 +1,25 @@
+#include "../tui.hpp"
+
+void print(tui::color::color4_t col, const char* str, bool blight=false){
+	if(blight){col.num+=10;}//col = tui::color::Blight(col);
+	col.set();
+	printf(str);
+}
+
+void test_4bitcol(bool b){
+	using namespace tui::color;
+	print(Red, "Red ", b);
+	print(Green, "Green ", b);
+	print(Yellow, "Yellow ", b);
+	print(Blue, "Blue ", b);
+	print(Magenta, "Magenta ", b);
+	print(Cyan, "Cyan ", b);
+	print(White, "White\n", b);
+}
+
+int main(int argc, char **argv){
+	using namespace tui;
+
+	test_4bitcol(false);
+	test_4bitcol(true);
+}
